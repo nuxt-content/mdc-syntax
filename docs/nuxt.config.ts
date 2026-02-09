7a11005115643c95139fdcfa3c8fbf7798ae870b
Farnabaz <farnabaz@gmail.com>
2026-02-09 17:17:46 +0100

    docs: update styles

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   extends: ['docus'],
+  css: ['~/assets/styles/main.css'],
   // @ts-expect-error - Nuxt Content types are not loaded
   content: {
     experimental: {
